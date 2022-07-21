1b30334b0a7a538892ac06b476b3c981c309d529
Zulfan Andria Putranto <93639634+zulfanandria@users.noreply.github.com>
2022-07-21 12:04:56 +0800

    Update Resizing and Rescalling Frames.py

--- Resizing and Rescalling Frames.py
+++ Resizing and Rescalling Frames.py
@@ -5,30 +5,30 @@ blank = np.zeros((500, 500, 3), dtype='uint8')
 cv.imshow('blank', blank)
 
 # Gambar
-# img = imread('OpenCV/Photo/123.jpg')
-# cv.imshow('gambar', img)
+img = imread('OpenCV/Photo/123.jpg')
+cv.imshow('gambar', img)
 
 # Warna
-# blank[200:300, 300:400] = 0,255,0
-# cv.imshow('green', blank)
+blank[200:300, 300:400] = 0,255,0
+cv.imshow('green', blank)
 
-# blank[:] = 0,0,255
-# cv.imshow('red', blank)
+blank[:] = 0,0,255
+cv.imshow('red', blank)
 
 # Segi-4
-# cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
-# cv.imshow('Rectangle', blank)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
 
 # Lingkaran
-# cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
-# cv.imshow('Circle', blank)
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', blank)
 
 # Garis
-# cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
-# cv.imshow('Garis', blank)
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
+cv.imshow('Garis', blank)
 
-# cv.line(blank, (100, 100), (300, 400), (255,255,255), thickness=3)
-# cv.imshow('Garis', blank)
+cv.line(blank, (100, 100), (300, 400), (255,255,255), thickness=3)
+cv.imshow('Garis', blank)
 
 # Text
 cv.putText(blank, 'Hello, my name is Zulfan', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
