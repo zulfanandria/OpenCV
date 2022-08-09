ac314a239919ab294e3937c11ff417321b66158c
Zulfan Andria Putranto <93639634+zulfanandria@users.noreply.github.com>
2022-08-09 21:10:06 +0700

    Update Resizing and Rescalling Frames.py

--- Resizing and Rescalling Frames.py
+++ Resizing and Rescalling Frames.py
@@ -1,5 +1,5 @@
-import cv2 as cv
-import numpy as np
+import cv2 as cv # Importing OpenCV
+import numpy as np # Importing Numpy
 
 blank = np.zeros((500, 500, 3), dtype='uint8')
 cv.imshow('blank', blank)
